9e4e7873cbe7babebc13c0d390e67989d2deca4b
jslucassf <jslucassf@gmail.com>
2020-08-04 12:03:05 -0300

    Allows drawing of multipolygons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { Map, Marker, CircleMarker, Polygon, TileLayer } from 'react-leaflet'
+import { Map, CircleMarker, Polyline, TileLayer } from 'react-leaflet'
 import './App.css';
 
 function App() {
-  const [centralPoint, setCentralPoint] = useState([-7.2281, -35.8739]);
-  const [pointsArray, setPointsArray] = useState([]);
-  const [zoom, setZoom] = useState(18);
+  const [centralPoint] = useState([-7.2281, -35.8739]);
+  const [pointsArray, setPointsArray] = useState([[]]);
+  const [zoom] = useState(18.5);
   const [isCircleEvent, setIsCircleEvent] = useState(false);
+  const [currentPolygon, setCurrentPolygon] = useState(0);
   const [wkt, setWkt] = useState("");
 
   useEffect(() => {
+    if(pointsArray[currentPolygon].length > 1){
+      const firstPoint = pointsArray[currentPolygon][0];
+      const lastPoint = pointsArray[currentPolygon][pointsArray[currentPolygon].length - 1];
+      if (firstPoint.lat === lastPoint.lat && firstPoint.lng === lastPoint.lng)  {
+        setCurrentPolygon(currentPolygon + 1);
+      };
+    }
+
     if(pointsArray.length === 1){
       setWkt(`POINT(${pointsArray[0].lat} ${pointsArray[0].lng})`);
     }else if(pointsArray.length === 2){
@@ -26,7 +35,11 @@ function App() {
     if(!isCircleEvent){
       const coords = event.latlng;
 
-      setPointsArray([...pointsArray, coords]);
+      if(currentPolygon === pointsArray.length - 1){
+        setPointsArray([...pointsArray.slice(0, -1), pointsArray.slice(-1)[0].concat(coords)]);
+      }else{
+        setPointsArray([...pointsArray, [coords]]);
+      }
     }
     setIsCircleEvent(false);
   }
@@ -35,6 +48,7 @@ function App() {
     <div className='container'>
       <Map  center={centralPoint} 
             zoom={zoom}
+            maxZoom={20}
             onclick={handleClick}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -43,35 +57,48 @@ function App() {
 
         <ul>
         {
-          pointsArray.map((point, ind) => 
-            <li key={ind}>
-              <CircleMarker
-                center={[point.lat, point.lng]}
-                radius={4}
-                weight={2}
-                color="#ef5350"
-                onclick={() => {
-                  setIsCircleEvent(true);
-                  setPointsArray(pointsArray.filter(p => p !== point));
-                }}
-                >
-              </CircleMarker>
-            </li>
+          pointsArray.map(polygon =>
+            polygon.map((point, ind) =>
+              <li key={ind}>
+                <CircleMarker
+                  center={[point.lat, point.lng]}
+                  radius={5} weight={2} color="#ef5350"
+                  onclick={(e) => {
+                    if(point === pointsArray[currentPolygon][0]){
+                      handleClick(e);
+                    }else{
+                      setPointsArray(pointsArray.map(polygon => polygon.filter(p => p !== point)));
+                    }
+                    setIsCircleEvent(true);
+                  }}
+                  >
+                </CircleMarker>
+              </li>
+            )
           )
         }
         </ul>
 
+        <ul>
         {
-          (pointsArray.length > 1) &&
-          <Polygon 
-            positions={pointsArray.map(point => [point.lat, point.lng])}
-            color="#ef5350"
-            >
-          </Polygon>
+          pointsArray.map(polygon =>
+            polygon.map((point, ind) => 
+            (polygon.length > 1) &&
+              <li key={ind}>
+              <Polyline 
+                positions={polygon.map(point => [point.lat, point.lng])}
+                color="#ef5350"
+                >
+              </Polyline>
+              </li>
+            )
+          )
         }
+        </ul>
 
         <button  className="reset" onClick={() => {
-            setPointsArray([]);
+            setPointsArray([[]]);
+            setCurrentPolygon(0);
           }}>
             Resetar
         </button>                
