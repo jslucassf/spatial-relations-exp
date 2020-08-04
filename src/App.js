57329ec491fcd0fde019be02c60ccef9d8fcea5f
jslucassf <jslucassf@gmail.com>
2020-08-04 17:09:08 -0300

    Adds suport to Multipolygons in the WKT representation

--- src/App.js
+++ src/App.js
@@ -19,15 +19,31 @@ function App() {
       };
     }
 
-    if(pointsArray.length === 1){
-      setWkt(`POINT(${pointsArray[0].lat} ${pointsArray[0].lng})`);
-    }else if(pointsArray.length === 2){
-      setWkt(`LINESTRING(${pointsArray[0].lat} ${pointsArray[0].lng}, ${pointsArray[1].lat} ${pointsArray[1].lng})`);
-    }else if(pointsArray.length > 2){
-      let newWkt = "POLYGON(";
+    let newWkt;
+    if(pointsArray.length === 1 && pointsArray[0].length > 0){
+      if(pointsArray[0].length === 1){
+        setWkt(`POINT(${pointsArray[0][0].lat} ${pointsArray[0][0].lng})`);
+      }else{
+        const firstPoint = pointsArray[0][0];
+        const lastPoint = pointsArray[0][pointsArray[0].length - 1];
+        if (firstPoint.lat === lastPoint.lat && firstPoint.lng === lastPoint.lng)  {
+          newWkt = "POLYGON(";
+        }else{
+          newWkt = "LINESTRING(";
+        }
+        pointsArray[0].map(point => newWkt += `${point.lat} ${point.lng},`);
+        setWkt(newWkt.replace(/.$/,")"));
+      }
+    }else if(pointsArray.length > 1){
+      newWkt = "MULTIPOLYGON(";
+      pointsArray.map(polygon => {
+        newWkt += "(";
+        polygon.map(point => newWkt += `${point.lat} ${point.lng},`);
+        newWkt = newWkt.replace(/.$/,"),");
+      });
+      newWkt = newWkt.replace(/.$/,")");
+      setWkt(newWkt);
 
-      pointsArray.map(point => newWkt += `${point.lat} ${point.lng},`);
-      setWkt(newWkt.replace(/.$/,")"));
     }
   }, [pointsArray])
 
@@ -100,7 +116,7 @@ function App() {
             setPointsArray([[]]);
             setCurrentPolygon(0);
           }}>
-            Resetar
+            Reset
         </button>                
       </Map>
 
